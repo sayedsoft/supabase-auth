6751a326bf7f3aef05aa99b24da648b7c0bf9e94
sayedsoft <yemen@sayedsoft.com>
2023-02-25 05:16:43 +0300

    supabase-auth

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "Int before Setup"
+echo " by AHMAD YAMAN SAYED"
+sudo apt -y upgrade
+sudo apt update
+sudo apt -y install curl gnupg ca-certificates lsb-release apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo "Setuping docker"
+sudo apt -y install docker-ce
+echo "Setuping docker-compose"
+sudo apt -y install docker-compose
+echo "Run docker-compose"
+docker-compose up -d
